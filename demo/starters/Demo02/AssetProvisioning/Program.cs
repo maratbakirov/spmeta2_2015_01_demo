dce97157c91570279bf85452c8c2520fb5fc3c5a
marat bakirov <marat.bakirov@igina.com.au>
2015-01-20 23:25:06 +0800

    added refernces to spmeta2

---
+++ demo/starters/Demo02/AssetProvisioning/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace AssetProvisioning
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                ReadSettings();
+                using (ClientContext ctx = GetAuthenticatedContext())
+                {
+                    ctx.Load(ctx.Web);
+                    ctx.ExecuteQuery();
+                    Console.WriteLine(ctx.Web.Url);
+                }
+            }
+            catch (Exception ex)
+            {
+                TraceHelper.TraceError("an error has occured, message:{0}", ex.Message);
+            }
+
+
+        }
+
+        static bool sharepointonline;
+
+        private static bool ReadSettings()
+        {
+            var sharepointonlinesetting = ConfigurationManager.AppSettings["SharepointOnline"];
+            bool.TryParse(sharepointonlinesetting, out sharepointonline);
+            return sharepointonline;
+        }
+
+        #region auth
+        private static ClientContext GetAuthenticatedContext()
+        {
+            var siteUrl = ConfigurationManager.AppSettings["siteurl"];
+            var context = new ClientContext(siteUrl);
+            if (sharepointonline)
+            {
+                SecureString password = GetPassword();
+                context.Credentials = new SharePointOnlineCredentials(ConfigurationManager.AppSettings["sharepointonlinelogin"],
+                    password);
+            }
+            return context;
+        }
+
+        private static SecureString storedPassword = null;
+        private static SecureString GetPassword()
+        {
+            if (storedPassword == null)
+            {
+                Console.WriteLine("Please enter your password");
+                storedPassword = GetConsoleSecurePassword();
+                Console.WriteLine();
+            }
+            return storedPassword;
+        }
+
+
+        private static SecureString GetConsoleSecurePassword()
+        {
+            SecureString pwd = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo i = Console.ReadKey(true);
+                if (i.Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+                else if (i.Key == ConsoleKey.Backspace)
+                {
+                    pwd.RemoveAt(pwd.Length - 1);
+                    Console.Write("\b \b");
+                }
+                else
+                {
+                    pwd.AppendChar(i.KeyChar);
+                    Console.Write("*");
+                }
+            }
+            return pwd;
+        }
+        #endregion
+
+    }
+}
+
---
+++ demo/starters/Demo02/AssetProvisioning/TraceHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetProvisioning
+{
+    public class TraceHelper
+    {
+        public static void TraceInformation(string str, params object[] args)
+        {
+            TraceInformation(ConsoleColor.Yellow, str, args);
+        }
+
+        public static void TraceInformation(ConsoleColor color, string str, params object[] args)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Trace.TraceInformation(str, args);
+            Console.ForegroundColor = oldColor;
+        }
+
+        public static void TraceError(string str, params object[] args)
+        {
+            var oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Trace.TraceError(str, args);
+            Console.ForegroundColor = oldColor;
+        }
+    }
+}
